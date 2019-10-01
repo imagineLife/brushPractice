23d98da17750ea00d5af76663b917951c06b1770
Eric Laursen <ericjlaursen@gmail.com>
2019-10-01 05:50:58 -0400

    introduces some hooks, uses prevState setter

--- src/App.js
+++ src/App.js
@@ -3,14 +3,14 @@ import "./index.css";
 import RadialChart from "./components/RadialChart";
 import Chart from "./components/Chart";
 
-class App extends Component {
-  state = {
+const App = () => {
+  let [state, setState] = React.useState({
     temps: {},
     city: "sf", // city whose temperatures to show
     range: [] // time range set by the brush
-  };
+  })
 
-  componentDidMount() {
+  React.useEffect(() => {
     Promise.all([
       fetch(`${process.env.PUBLIC_URL || ""}/sf.json`),
       fetch(`${process.env.PUBLIC_URL || ""}/ny.json`)
@@ -20,57 +20,68 @@ class App extends Component {
         sf.forEach(day => (day.date = new Date(day.date)));
         ny.forEach(day => (day.date = new Date(day.date)));
 
-        this.setState({ temps: { sf, ny } });
+        setState(prevState => {
+          let newObj = { temps: { sf, ny } }
+          return {...prevState, ...newObj}
+        });
       });
-  }
+  }, [])
 
-  updateCity = e => {
-    this.setState({ city: e.target.value });
+  let updateCity = e => {
+    let newObj = { city: e.target.value }
+    setState(prevState => { 
+      return {...prevState, ...newObj}
+    });
   };
 
-  updateRange = range => {
-    this.setState({ range });
+  let updateRange = range => {
+    let newObj = { range }
+    setState(prevState => {
+      return {...prevState, ...newObj}
+    });
   };
 
-  render() {
-    const data = this.state.temps[this.state.city];
+  const data = state.temps[state.city];
 
-    return (
-      <div className="App">
-        <h1>
-          2017 Temperatures for
-          <select name="city" onChange={this.updateCity}>
-            {[
-              { label: "San Francisco", value: "sf" },
-              { label: "New York", value: "ny" }
-              // {label: 'Amsterdam', value: 'am'},
-            ].map(option => {
-              return (
-                <option key={option.value} value={option.value}>
-                  {option.label}
-                </option>
-              );
-            })}
-          </select>
-        </h1>
-        <p>
-          *warning: these are <em>not</em> meant to be good examples of data
-          visualizations,<br />
-          but just to show the possibility of using D3 and React*
-        </p>
-        <RadialChart data={data} range={this.state.range} />
-        <Chart data={data} 
-          range={this.state.range} 
-          updateRange = {this.updateRange}/> 
-        <p>
-          (Weather data from{" "}
-          <a href="wunderground.com" target="_new">
-            wunderground.com
-          </a>)
-        </p>
-      </div>
-    );
-  }
+  console.log('%c - - - App - - -', 'background-color: orange; color: white;')
+  console.log('state')
+  console.log(state)
+  
+  return (
+    <div className="App">
+      <h1>
+        2017 Temperatures for
+        <select name="city" onChange={updateCity}>
+          {[
+            { label: "San Francisco", value: "sf" },
+            { label: "New York", value: "ny" }
+            // {label: 'Amsterdam', value: 'am'},
+          ].map(option => {
+            return (
+              <option key={option.value} value={option.value}>
+                {option.label}
+              </option>
+            );
+          })}
+        </select>
+      </h1>
+      <p>
+        *warning: these are <em>not</em> meant to be good examples of data
+        visualizations,<br />
+        but just to show the possibility of using D3 and React*
+      </p>
+      <RadialChart data={data} range={state.range} />
+      <Chart data={data} 
+        range={state.range} 
+        updateRange = {updateRange}/> 
+      <p>
+        (Weather data from{" "}
+        <a href="wunderground.com" target="_new">
+          wunderground.com
+        </a>)
+      </p>
+    </div>
+  );
 }
 
 export default App;
