43397808fc236d433cc56e51640d651f74195435
Eric Laursen <ericjlaursen@gmail.com>
2019-10-01 06:18:35 -0400

    renders without brush, again

--- src/components/Chart/Hooks.js
+++ src/components/Chart/Hooks.js
@@ -1,58 +1,32 @@
 import React, { Component } from "react";
 import * as d3 from "d3";
 
-const width = 650;
-const height = 400;
-const margin = { top: 20, right: 5, bottom: 20, left: 35 };
-
 class Chart extends Component {
   state = {
-    bars: []
+    bars: [],
+    width :650,
+    height :400,
+    margin :{ top: 20, right: 5, bottom: 20, left: 35 },
+    xScale: d3.scaleTime(),
+    yScale: d3.scaleLinear(),
+    colorScale : d3.scaleSequential(d3.interpolateSpectral)
   };
 
   xAxis = d3.axisBottom();
   yAxis = d3.axisLeft();
 
-  // static getDerivedStateFromProps(nextProps, prevState) {
-  //   //get next props
-  //   const { data, range } = nextProps;
-  //   if (!data) return {};
-
-  //   //build scale-ranges
-  //   const xScale = d3.scaleTime().range([margin.left, width - margin.right]);
-  //   const yScale = d3.scaleLinear().range([height - margin.bottom, margin.top]);
-  //   const colorScale = d3.scaleSequential(d3.interpolateSpectral);
-
-  //   //set domain vals
-  //   const timeDomain = d3.extent(data, d => d.date);
-  //   const tempMax = d3.max(data, d => d.high);
-  //   const [minAvg, maxAvg] = d3.extent(data, d => d.avg);
-    
-  //   //set scale domains
-  //   xScale.domain(timeDomain);
-  //   yScale.domain([0, tempMax]);
-  //   colorScale.domain([maxAvg, minAvg]);
-
-  //   // calculate x and y for each rectangle
-  //   const bars = data.map(d => {
-  //     const isColored = !range || (range[0] < d.date && d.date < range[1]);
-  //     const y1 = yScale(d.high);
-  //     const y2 = yScale(d.low);
-  //     return {
-  //       x: xScale(d.date),
-  //       y: y1,
-  //       height: y2 - y1,
-  //       fill: isColored ? colorScale(d.avg) : "#ccc"
-  //     };
-  //   });
-  //   return { bars, xScale, yScale };
-  // }
   componentDidMount() {
-    this.brush = d3
+    console.log('%c CDM', 'background-color: black; color: white;')
+    // console.log('this.state')
+    // console.log(this.state)
+    // console.log('this.props')
+    // console.log(this.props)
+    
+    const brushFn = d3
       .brushX()
       .extent([
-        [margin.left, margin.top], //top left
-        [width - margin.right, height - margin.top] //bottom right
+        [this.state.margin.left, this.state.margin.top], //top left
+        [this.state.width - this.state.margin.right, this.state.height - this.state.margin.top] //bottom right
       ])
       .on("end", () => {
         const [minX, maxX] = d3.event.selection;
@@ -62,7 +36,11 @@ class Chart extends Component {
         ];
         this.props.updateRange(range);
       });
-    d3.select(this.refs.brush).call(this.brush);
+
+    console.log('this.refs.brush')
+    console.log(this.refs.brush)
+    
+    d3.select(this.refs.brush).call(brushFn);
   }
   componentDidUpdate() {
     this.xAxis.scale(this.state.xScale);
@@ -77,6 +55,9 @@ class Chart extends Component {
     console.log(this.state)
     console.log('this.props')
     console.log(this.props)
+    console.log('this.refs')
+    console.log(this.refs)
+    
     
     let {data, range } = this.props
 
@@ -84,8 +65,8 @@ class Chart extends Component {
       return <p>loading data...</p>
     }
         //build scale-ranges
-    const xScale = d3.scaleTime().range([margin.left, width - margin.right]);
-    const yScale = d3.scaleLinear().range([height - margin.bottom, margin.top]);
+    const xScale = this.state.xScale.range([this.state.margin.left, this.state.width - this.state.margin.right]);
+    const yScale = this.state.yScale.range([this.state.height - this.state.margin.bottom, this.state.margin.top]);
     const colorScale = d3.scaleSequential(d3.interpolateSpectral);
 
     //set domain vals
@@ -112,12 +93,12 @@ class Chart extends Component {
     });
     
     return (
-      <svg width={width} height={height}>
+      <svg width={this.state.width} height={this.state.height}>
         {this.state.bars.map(d => (
           <rect x={d.x} y={d.y} width={2} height={d.height} fill={d.fill} />
         ))}
-        <g ref="xAxis" transform={`translate(0, ${height - margin.bottom})`} />
-        <g ref="yAxis" transform={`translate(${margin.left}, 0)`} />
+        <g ref="xAxis" transform={`translate(0, ${this.state.height - this.state.margin.bottom})`} />
+        <g ref="yAxis" transform={`translate(${this.state.margin.left}, 0)`} />
         <g ref="brush" />
       </svg>
     );
